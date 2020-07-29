7a8a5000abce18085394226c52702cd9e4454c77
benzene25 <54492029+benzene25@users.noreply.github.com>
2020-07-29 10:27:59 +0530

    Add files via upload

---
+++ container.php
@@ -0,0 +1,67 @@
+ <div id="content">
+			    <div>
+			          <img src="images/img.png" style="float:left; margin-left:-200px";/>
+					</div>
+					<div id="form2">
+					    <form action="" method="post">
+						
+						   <table>
+						
+						        <tr>
+								       <td align="right"><strong>Name:</strong></td>
+									   <td><input type="text" name="u_name" required="required" placeholder="write your name"/></td>
+						        </tr> 
+								
+								<tr>
+								       <td align="right"><strong>Password:</strong></td>
+									   <td><input type="pasword" name="u_pass" required="required" placeholder="enter a password"/></td>
+						        </tr> 
+								
+								<tr>
+								       <td align="right"><strong>Email:</strong></td>
+									   <td><input type="email" name="u_email" required="required" placeholder="write your email"/></td>
+						        </tr> 
+								
+								<tr>
+								       <td align="right"><strong>Branch:</strong></td>
+									   <td>
+									   <select name="u_branch">
+									   <option>select your branch</option>
+									   <option>CSE</option>
+									   <option>ECE</option>
+									   <option>MECH</option>
+									   <option>CIVIL</option>
+									   <option>ELECTRICAL</option>
+									   </select>
+									   </td>
+						        </tr> 
+								
+								<tr>
+								       <td align="right"><strong>Status:</strong></td>
+									  <td>
+									        <select name="u_status">
+											   <option>select your status</option>
+											   <option>Student</option>
+											   <option>Faculty</option>
+									  </td>
+						        </tr> 
+								
+								<tr>
+								       <td align="right"><strong>Birthday:</strong></td>
+									   <td><input type="date" name="u_name" required="required  "/></td>
+						        </tr> 
+								
+								<tr>
+								       <td colspan="6">
+									   <button name="sign_up">Sign Up</button>
+									   </td>
+						        </tr> 
+								
+						
+						
+						  </table>
+						</form>
+					</div>
+					
+			 </div>
+			 </div>
---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+
+
+<html> 
+    <head>
+	    <title>Sugestie!</title>
+		<link rel="stylesheet" href="styles/style.css" media="all"/>
+    </head>
+	
+	
+<body>
+
+    <div class="container">
+	
+	     <div id="header_wrapper">
+		     <div id="header">
+			     <img src="images/logo1.png" style="float:left;"/>
+				 <form method="post" action="" id="form1">
+				 <strong>EMAIL:</strong>
+				 <input type="email" name="email" placeholder="Email" required="required"/>
+				 <strong>PASSWORD::</strong>
+				 <input type="password" name="pass" placeholder="*****" required="required"/>
+				 <button name="login">Login</button>
+				 </form>
+				 </div>
+			 </div>
